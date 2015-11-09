8bd30549e305b880f3671c5712e957a39276740c
David Turissini <david@Davids-MacBook-Pro.local>
2015-11-08 21:47:06 -0700

    port for heroku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 const server = require('data-driven-design-server-bootstrap');
 // token: 2887648e-f89a-4465-b38a-9a7b417e22db
 
-server(4000, [{
+server(process.env.PORT || 4000;, [{
     "method": "get",
     "path": "/users/:user_id/abtests",
     "handler": "./src/abtests/index"
