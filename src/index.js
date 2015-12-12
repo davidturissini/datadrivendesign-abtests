477f737d65a0059848ebef932d7bcb7f0e4f871a
David Turissini <david@Davids-MacBook-Pro.local>
2015-12-12 16:18:15 -0700

    Changing stream. adding json response type

--- src/index.js
+++ src/index.js
@@ -11,116 +11,121 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/ddd-test');
 
+const port = process.env.PORT || 4000;
 
-rx.Observable.create(function (o) {
-    const expressApp = express();
+rx.Observable.return(port)
+    .map((port) => {
 
-    expressApp.use(function (req, res, next) {
-        const headers = req.headers;
-        const contentType = headers['Content-Type'];
+        const expressApp = express();
 
-        if (!contentType) {
-            headers['Content-Type'] = 'application/json';
-        }
+        expressApp.use(function (req, res, next) {
+            const headers = req.headers;
+            const contentType = headers['Content-Type'];
 
-        next();
+            if (!contentType) {
+                headers['Content-Type'] = 'application/json';
+            }
 
-    });
+            next();
 
-    // parse application/json
-    expressApp.use(bodyParser.json());
-
-    expressApp.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "authentication, Origin, Content-Type, Accept");
-      next();
-    });
-
-    expressApp.use(cors());
-
-    o.onNext(expressApp);
-    o.onCompleted();
-})
-
-.flatMapLatest((expressApp) => {
-    const routes = [{
-        method: 'post',
-        path: '/users',
-        handler: require('./controller/user/create')
-    },{
-        method: 'post',
-        path: '/userSession',
-        handler: require('./controller/userSession/create')
-    },{
-        method: 'delete',
-        path: '/userSession',
-        handler: require('./controller/userSession/delete')
-    },{
-        method: 'get',
-        path: '/users/:user_id',
-        handler: require('./controller/user/show')
-    },{
-        method: 'get',
-        path: '/users/:user_id/abtests',
-        handler: require('./controller/abtest/index')
-    },{
-        method: 'post',
-        path: '/users/:user_id/abtests',
-        handler: require('./controller/abtest/create')
-    },{
-        method: 'post',
-        path: '/abtests/:abtest_id/impressions',
-        handler: require('./controller/impression/create')
-    },{
-        method: 'post',
-        path: '/abtests/:abtest_id/conversions',
-        handler: require('./controller/conversion/create')
-    }];
-
-    return rx.Observable.create(function (o) {
-        routes.forEach(function (data) {
-
-            expressApp[data.method](data.path, function (req, res) {
-                data.handler(req)
-                    .subscribe(function (data) {
-                        o.onNext({
-                            data: {
-                                data: data
-                            },
-                            res: res
-                        });
-                    }, function (error) {
-                        const errorMessage = error.message ? error.message : error;
-
-                        o.onNext({
-                            data: {
-                                error: {
-                                    message: errorMessage
-                                }
-                            },
-                            res: res
-                        });
-                    })
-            });
-
-        
         });
 
-        const port = process.env.PORT || 4000;
+        // parse application/json
+        expressApp.use(bodyParser.json());
+
+        expressApp.use(function(req, res, next) {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "authentication, Origin, Content-Type, Accept");
+          next();
+        });
+
+        expressApp.use(cors());
+
+
         expressApp.listen(port);
         console.log(`Server started on port ${port}`);
+
+        return expressApp;
+    })
+
+    .flatMapLatest((expressApp) => {
+        const routes = [{
+            method: 'post',
+            path: '/users',
+            handler: require('./controller/user/create')
+        },{
+            method: 'post',
+            path: '/userSession',
+            handler: require('./controller/userSession/create')
+        },{
+            method: 'delete',
+            path: '/userSession',
+            handler: require('./controller/userSession/delete')
+        },{
+            method: 'get',
+            path: '/users/:user_id',
+            handler: require('./controller/user/show')
+        },{
+            method: 'get',
+            path: '/users/:user_id/abtests',
+            handler: require('./controller/abtest/index')
+        },{
+            method: 'post',
+            path: '/users/:user_id/abtests',
+            handler: require('./controller/abtest/create')
+        },{
+            method: 'post',
+            path: '/abtests/:abtest_id/impressions',
+            handler: require('./controller/impression/create')
+        },{
+            method: 'post',
+            path: '/abtests/:abtest_id/conversions',
+            handler: require('./controller/conversion/create')
+        }];
+
+
+        return rx.Observable.fromArray(routes).flatMap((routeData) => {
+            return rx.Observable.create(function (o) {
+
+                expressApp[routeData.method](routeData.path, function (req, res) {
+                    routeData.handler(req)
+                        .subscribe(function (data) {
+                            o.onNext({
+                                data: {
+                                    data: data
+                                },
+                                res: res
+                            });
+                        }, function (error) {
+                            const errorMessage = error.message ? error.message : error;
+
+                            o.onNext({
+                                data: {
+                                    error: {
+                                        message: errorMessage
+                                    }
+                                },
+                                res: res
+                            });
+                        })
+                });
+
+            
+            })
+        });
+
+        
+    })
+
+    .subscribe(function (data) {
+        const res = data.res;
+        const json = data.data;
+
+        const jsonString = JSON.stringify(json);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.write(jsonString);
+        res.end();
+    }, function (err) {
+        console.log(err);
     });
-
-    
-})
-.subscribe(function (data) {
-    const res = data.res;
-    const json = data.data;
-
-    const jsonString = JSON.stringify(json);
-
-    res.write(jsonString);
-    res.end();
-}, function (err) {
-    console.log(err);
-});
